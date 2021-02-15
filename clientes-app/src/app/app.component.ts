4c0631c25700e50617274035b4baa544e2571b4a
German Sanchez Pastor <luqh555@gmail.com>
2021-02-15 11:20:21 +0100

    Primeros pasos

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+  curso: string = 'Cursillo Angularcillo';
+  profesor: string = 'El risas';
+  alumno: string = 'El maquina';
 }
