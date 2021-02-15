df2f4fcbc28d7cdeb212432a47f5df3a7c81df98
German Sanchez Pastor <luqh555@gmail.com>
2021-02-15 16:22:30 +0100

    nueva tabla con datos de json

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  templateUrl: './clientes.component.html'
+  
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Cliente[] = [
+{id:1,nombre:'German',apellido:'Sanchez',createAt:'2077',email:'jijijojo@juju.com'},
+{id:1,nombre:'loloan',apellido:'Sqwwez',createAt:'2127',email:'jasdjo@juju.com'},
+{id:1,nombre:'kikian',apellido:'araschez',createAt:'20377',email:'jijgfdghojo@juju.com'},
+{id:1,nombre:'Gwfgman',apellido:'Sbnbchez',createAt:'177',email:'jhgfjojo@juju.com'},
+{id:1,nombre:'Geaawn',apellido:'Sdfz',createAt:'2037',email:'wqwejojo@juju.com'}
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
