9687bf91a8c8d3345dc24d5af275acb0419dd4a6
German Sanchez Pastor <luqh555@gmail.com>
2021-02-18 10:12:48 +0100

    Boton editar en funcionamiento, cors corregido

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Cliente } from './cliente';
@@ -9,10 +9,25 @@ import { CLIENTES } from './clientes.json';
 })
 export class ClienteService {
 private urlEndpoint:string ='http://localhost:8080/api/clientes';
+
+private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
     //return of (CLIENTES);
     return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente:Cliente) : Observable<Cliente>{
+
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers:this.httpHeaders})
+  }
+
+  getCliente(id): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`)  
+  }
+
+  update(cliente:Cliente): Observable<Cliente>{
+    return this.http.put<Cliente>(`${this.urlEndpoint}/${cliente.id}`, cliente,{headers: this.httpHeaders});
   }
 }
--- clientes-app/src/app/clientes/form.component.ts
+++ clientes-app/src/app/clientes/form.component.ts
@@ -1,22 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
- 
+
 })
 export class FormComponent implements OnInit {
 
   public cliente: Cliente = new Cliente();
-  public titulo:string="Crear Cliente";
-  constructor() { }
+  public titulo: string = "Crear Cliente";
+  constructor(private clienteService: ClienteService, private router: Router, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.cargarCliente();
   }
 
-  public create():void{
-    console.log("Clicked!");
-    console.log(this.cliente);
+public cargarCliente(): void{
+this.activatedRoute.params.subscribe(params=>{
+  let id = params['id']
+  if(id){
+    this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
   }
+} )
+}
+
+  public create(): void {
+    this.clienteService.create(this.cliente).subscribe(cliente => {
+      this.router.navigate(['/clientes'])
+      Swal.fire(  'Nuevo cliente',  'Cliente '+this.cliente.nombre+ ' creado con exito',  'success');
+    }
+
+    )
+
+  }
+
+public update():void{
+  this.clienteService.update(this.cliente).subscribe(cliente =>{
+    this.router.navigate(['/clientes'])
+    Swal.fire(  'Cliente Actualizado',  'Cliente '+this.cliente.nombre+ ' actualizado con exito',  'success');
+  })
+}
+
 }
